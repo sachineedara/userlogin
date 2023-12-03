43bed9a80a358fcd79396109bf35201b0b5ff29f
sachineedara <119740726+sachineedara@users.noreply.github.com>
2023-12-03 11:49:54 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+const app = express();              
+const port = 5000;                  
+
+
+app.get('/', (req, res) => {        
+    res.sendFile('index.html', {root: __dirname});      
+                                                        
+});
+app.get('/pfolio.html', (req, res) => {        
+    res.sendFile('pfolio.html', {root: __dirname});      
+                                                        
+});
+app.listen(port, () => {            
+    console.log(`Now listening on port ${port}`); 
+});
